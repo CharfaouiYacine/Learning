8501c03d9d4703d6377c3ea8f2ad197eeb8afd3b
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-02-27 12:39:47 +0100

    learning gui in python

---
+++ learning_GUI.py
@@ -0,0 +1,7 @@
+from tkinter import *
+fouad = Tk()
+fouad.geometry("420x420") # Change the Height and Width of our window
+fouad.title("Son_Fouad") # changes the title of the window
+fouad.config(background="#5cfcff") # changes the background color using color name or hexadecimal code
+
+fouad.mainloop() # this code will display a window on computer screen
