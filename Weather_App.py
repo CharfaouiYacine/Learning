f5dd5ae13a1b4e28fbdab25c0dbc19280dac92b1
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-03-21 22:30:48 +0100

    get the weather using http request

---
+++ Weather_App.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key = "4630e317616448e09e4145345262103"
+def get_weather(city):
+    try:
+        response = requests.get(f'https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}')
+        data = response.json()
+        print(f"------{city} Weather-------\nThe weather is: {data['current']['condition']['text']}\nTemperature in celsius: {data['current']['temp_c']}\nTemperature in fahrenheit: {data['current']['temp_f']}\nHumidity is:{data['current']['humidity']}")
+    except Exception:
+        response= requests.get(f'https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}')
+        print(f"didn't find anything , Error: {response}")
+def main():
+    city = input("Please enter your City: ").title()
+    get_weather(city)
+
+if __name__ == '__main__':
+    main()
+
+"""You enter a city and get the weather and temperature back from the weather API"""
